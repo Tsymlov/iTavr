02b2f53498be9eec53dbe8c0a090ffedc29d6d4d
Alexey Tsymlov <alexeytsymlov@Alexeys-Mac-mini.local>
2014-08-25 18:28:49 +0400

    Initial Commit

---
+++ iTavr/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iTavr
+//
+//  Created by Alexey Tsymlov on 25/08/14.
+//  Copyright (c) 2014 alexeytsymlov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iTavrTests/iTavrTests.m
@@ -0,0 +1,34 @@
+//
+//  iTavrTests.m
+//  iTavrTests
+//
+//  Created by Alexey Tsymlov on 25/08/14.
+//  Copyright (c) 2014 alexeytsymlov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iTavrTests : XCTestCase
+
+@end
+
+@implementation iTavrTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
